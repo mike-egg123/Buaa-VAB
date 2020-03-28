c91756b7f0c37405382e04e687e8d553b19a974e
mike-egg123 <qrn20000312@163.com>
2020-03-28 22:59:47 +0800

    big changes

--- VAB/Books/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Book
-
-
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ('Book_name','Book_auth','Book_intro','Book_img')
